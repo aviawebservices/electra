733cd4814b0b02dab6b71782bacbdbdc453426e4
Arjan Gribizi <aviawebservices@gmail.com>
2020-02-12 14:08:20 +0100

    Currency seeder me listen e monedhave

---
+++ database/migrations/2020_02_12_105742_create_currencies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCurrenciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('currencies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('iso_code', 10);
+            $table->string('name');
+            $table->string('symbol', 10);
+            $table->string('html_entity');
+            $table->string('subunit');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('currencies');
+    }
+}
